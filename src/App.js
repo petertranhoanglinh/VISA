3eb4213511633d8ff77f47105087d908719e9207
petertranhoanglinh <petertranhoanglinh@gmail.com>
2022-07-28 15:45:44 +0700

    add visa
    1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Nav from './components/Nav'
+import Row from './components/Row'
+import {Routes, Route} from 'react-router-dom'
+function App(){
+  return (
+    <div>
+      <Nav></Nav>
+      <Row></Row>
+      <Routes>
+          
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
